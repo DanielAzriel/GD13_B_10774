bb8856ff54db2a414136234896c9b4479386fbbb
DanielAzriel <danielaazrl@gmail.com>
2022-12-01 08:42:51 +0700

    GD13_B_10774

---
+++ app/src/main/java/com/gd/gd11_b_10774/models/Mahasiswa.kt
@@ -0,0 +1,8 @@
+package com.gd.gd11_b_10774.models
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mahasiswa (var nama: String, var npm: String, var fakultas: String, var prodi: String) {
+    var id: Long? = null
+}
